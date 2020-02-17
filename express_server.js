5be0284a3ac2422df973be42392d2a6288616e60
NoahNovick <nnovickfaille1@gmail.com>
2020-02-17 14:26:44 -0500

    downloaded body parser to accept post

--- express_server.js
+++ express_server.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
 const urlDatabase = {
   b2xVn2: "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
@@ -10,6 +13,10 @@ const urlDatabase = {
 app.get("/urls", (req, res) => {
   let templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
+});
+
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
 });
 
 app.get("/urls/:shortURL", (req, res) => {
