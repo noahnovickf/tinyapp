c29261f0b3f6cb83f7fe41ad71ee1eb7213ab920
NoahNovick <nnovickfaille1@gmail.com>
2020-02-17 15:47:01 -0500

    updated new urls so it redirects

--- express_server.js
+++ express_server.js
@@ -26,7 +26,11 @@ app.get("/urls", (req, res) => {
 
 app.post("/urls", (req, res) => {
   console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
+  let longURL = req.body.longURL;
+  let shortURL = generateRandomString();
+  urlDatabase[shortURL] = longURL;
+  //console.log(urlDatabase);
+  res.redirect("/urls");
 });
 
 app.get("/urls/new", (req, res) => {
