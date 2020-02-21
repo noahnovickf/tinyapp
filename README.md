863993d48c1eeae139de1a2dac80b2fe84c200ab
NoahNovick <nnovickfaille1@gmail.com>
2020-02-20 19:50:06 -0500

    read me with screenshots

---
+++ README.md
@@ -0,0 +1,25 @@
+**This application requires :**
+
+- bcryptjs
+- body-parser
+- cookie-session
+- ejs
+- express
+
+**Please make sure you have these dependencies.**
+
+Otherwise, this app is a full stack web app built with Node and Express that :
+
+Generates a random short URL for any and all valid URLS you can think of. This offers a quick way to access your favourite websites even faster, whilst also being able to share your truncated URLS.
+
+**How to get started:**
+
+- Install all dependencies using npm install command
+- run the server using node express_server.js command
+
+The downloaded app should look something like this:
+
+**REGISTER**
+!["Screenshot of register page"](https://github.com/noahnovickf/tinyapp/blob/master/docs/register-page.png)
+**URLS PAGE**
+!["Screenshot of URLs page"](https://github.com/noahnovickf/tinyapp/blob/master/docs/url-page.png)
--- express_server.js
+++ express_server.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 const cookieSession = require("cookie-session");
 const bcrypt = require("bcryptjs");
 const { checkAuthentification, generateRandomString } = require("./helpers");
+//app.uses
 app.use(
   cookieSession({
     name: "user_id",
@@ -13,12 +14,13 @@ app.use(
 );
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
+
 //listening on which port
 app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
+  console.log(`Tiny App listening on port ${PORT}!`);
 });
-//functions
 
+//functions
 let urlsForUser = id => {
   let userURLdatabase = {};
   for (let shortURL in urlDatabase) {
@@ -28,22 +30,21 @@ let urlsForUser = id => {
   }
   return userURLdatabase;
 };
-
-const checkEmail = email => {
-  for (let id in users) {
-    if (email === users[id].email) {
-      return true;
-    }
-  }
-};
 //empty object to be filled with users
 const users = {};
-//will be empty object for storing websites and for userURLdatabase to reference
-const urlDatabase = {
-  b6UTxQ: { longURL: "https://www.tsn.ca", userID: "0qoJOq" },
-  i3BoGr: { longURL: "https://www.google.ca", userID: "345" }
-};
+//empty string to be filled with urls
+const urlDatabase = {};
 
+//route to redirects home page to login if logged out, otherwise redirects to urls
+app.get("/", (req, res) => {
+  res.redirect("/urls");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+//routes urls, renders index with table of urldatabase info
 app.get("/urls", (req, res) => {
   if (req.session.user_id) {
     let templateVars = {
@@ -56,16 +57,16 @@ app.get("/urls", (req, res) => {
     res.redirect("/login");
   }
 });
-
+// publishes urls to table
 app.post("/urls", (req, res) => {
   let shortURL = generateRandomString();
   urlDatabase[shortURL] = {
     longURL: req.body.longURL,
     userID: req.session.user_id
   };
-  res.redirect("/urls");
+  res.redirect(`/urls/${shortURL}`);
 });
-
+// create new urls only if the user is logged in, using functionality from new.ejs
 app.get("/urls/new", (req, res) => {
   if (req.session.user_id) {
     let templateVars = { user: users[req.session["user_id"]] };
@@ -74,41 +75,24 @@ app.get("/urls/new", (req, res) => {
     res.redirect("/login");
   }
 });
-
+// route to check if user is logged in and shortURL exists, brings you to a specific url page
 app.get("/urls/:shortURL", (req, res) => {
-  if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
-    let templateVars = {
-      shortURL: req.params.shortURL,
-      longURL: urlDatabase[req.params.shortURL].longURL,
-      user: users[req.session["user_id"]]
-    };
-    res.render("urls_show", templateVars);
+  if (urlDatabase[req.params.shortURL]) {
+    if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
+      let templateVars = {
+        shortURL: req.params.shortURL,
+        longURL: urlDatabase[req.params.shortURL].longURL,
+        user: users[req.session["user_id"]]
+      };
+      res.render("urls_show", templateVars);
+    } else {
+      res.sendStatus(403);
+    }
   } else {
-    res.sendStatus(403);
+    res.sendStatus(404);
   }
 });
-
-app.get("/u/:shortURL", (req, res) => {
-  res.redirect(urlDatabase[req.params.shortURL].longURL);
-});
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
-});
-
-app.get("/urls.json", (req, res) => {
-  res.json(urlDatabase);
-});
-
-app.post("/urls/:shortURL/delete", (req, res) => {
-  if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
-    delete urlDatabase[req.params.shortURL];
-    res.redirect("/urls");
-  } else {
-    res.sendStatus(403);
-  }
-});
-
+// route to check if url is linked w current user to push new url into the url database
 app.post("/urls/:shortURL", (req, res) => {
   if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
     urlDatabase[req.params.shortURL] = {
@@ -120,7 +104,20 @@ app.post("/urls/:shortURL", (req, res) => {
     res.sendStatus(403);
   }
 });
-
+// route to go to long url page
+app.get("/u/:shortURL", (req, res) => {
+  res.redirect(`http://${urlDatabase[req.params.shortURL].longURL}`);
+});
+// enacts delete function, returns to url table page
+app.post("/urls/:shortURL/delete", (req, res) => {
+  if (urlDatabase[req.params.shortURL].userID === req.session.user_id) {
+    delete urlDatabase[req.params.shortURL];
+    res.redirect("/urls");
+  } else {
+    res.sendStatus(403);
+  }
+});
+// calls login page
 app.get("/login", (req, res) => {
   const templateVars = {
     email: req.body.email,
@@ -129,22 +126,26 @@ app.get("/login", (req, res) => {
   };
   res.render("urls_login", templateVars);
 });
-
+// checks validity of login info and if so, sets cookies
 app.post("/login", (req, res) => {
-  let user = checkAuthentification(req.body.email, users);
-  if (bcrypt.compareSync(req.body.password, user.password)) {
-    req.session.user_id = user.id;
-    res.redirect("/urls");
-  } else {
+  if (!checkAuthentification(req.body.email, users)) {
     res.sendStatus(403);
+  } else {
+    let user = checkAuthentification(req.body.email, users);
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      req.session.user_id = user.id;
+      res.redirect("/urls");
+    } else {
+      res.sendStatus(403);
+    }
   }
 });
-
+//redirects to urls, but logs user out
 app.post("/logout", (req, res) => {
   res.clearCookie("user_id");
   res.redirect("/urls");
 });
-
+// calls register page
 app.get("/register", (req, res) => {
   const templateVars = {
     email: req.body.email,
@@ -153,12 +154,12 @@ app.get("/register", (req, res) => {
   };
   res.render("urls_register", templateVars);
 });
-
+// checks if new users info already exists, and if not, creates new user in the user database
 app.post("/register", (req, res) => {
   if (!req.body.email || !req.body.password) {
     res.sendStatus(400);
   }
-  if (checkEmail(req.body.email)) {
+  if (checkAuthentification(req.body.email, users)) {
     res.sendStatus(400);
   } else {
     const id = generateRandomString();
